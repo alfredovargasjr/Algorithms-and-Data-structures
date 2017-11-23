41d90574058a330bcb620d79415b36c2f47d2b74
Alfredo Vargas <vargas.alfredojr@gmail.com>
2017-11-23 14:50:26 -0800

    Algorithm, Create Max Heap

---
+++ CECS_328 - Lab 5/src/Lab4_Heaps.java
@@ -0,0 +1,19 @@
+public class Lab4_Heaps {
+
+    public static void main(String[] args){
+
+    }
+
+    public void build_MaxHeap(int[] array){
+
+    }
+
+    public void max_Heapify(int[] array, int root){
+        if(root - (array.length - 1) == 0)
+            return;
+        int left = 2 * root;
+        int right = left + 1;
+        if(array[root] < )
+        max_Heapify(array, );
+    }
+}
