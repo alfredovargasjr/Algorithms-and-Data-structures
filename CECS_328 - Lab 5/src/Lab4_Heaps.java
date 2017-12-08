19abfb5c8e4908438e85c6aa1b6067a2b78f6099
Alfredo Vargas <vargas.alfredojr@gmail.com>
2017-12-08 00:53:16 -0800

    Final implementation of heap_sort

--- CECS_328 - Lab 5/src/Lab4_Heaps.java
+++ CECS_328 - Lab 5/src/Lab4_Heaps.java
@@ -12,49 +12,96 @@ public class Lab4_Heaps {
         for(int i = 0; i < n; i++){
             array[i] = randInt(-10000,10000);
         }
-        array = build_MaxHeap(array, array.length);
-        System.out.println(Arrays.toString(array));
+        array = build_MaxHeap(array, array.length, array.length);
         heap_sort(array);
         System.out.println(Arrays.toString(array));
 
     }
 
+    /**
+     * Sort the array, in ascending order, using heap sort
+     *
+     * Gets a max heap and swaps the root with last leaf node,
+     *      ignore the swapped root, (deleting the old root from the tree)
+     *      array no longer a max heap so we will build a new max heap.
+     *      Max heapify will be called until the array is a max heap.
+     *      repeats this process until there are no more nodes in the heap (right is 0)
+     *
+     * @param array
+     */
     public static void heap_sort(int[] array){
         int right = array.length;
         while(right > 0){
-            System.out.println(right);
+            System.out.println(Arrays.toString(array));
             swap(array, 0, right - 1);
-            build_MaxHeap(array, right--);
+            right--;
+            build_MaxHeap(array, right, right);
         }
     }
 
-    public static int[] build_MaxHeap(int[] array, int  right){
-        for(int i = right; i > 0; i--){
-            max_Heapify(array,i);
+    public static void changeOrder(int[] array){
+        int left = 0;
+        int right = array.length - 1;
+        while(left < right){
+            swap(array, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    /**
+     *
+     * @param array
+     * @param parent    is current node at which max_heapify is being done
+     * @param right     the last leaf node, ignore the swapped roots
+     * @return          max_heap of array
+     */
+    public static int[] build_MaxHeap(int[] array, int parent, int  right){
+        for(int i = parent; i > 0; i--){
+            max_Heapify(array, i, right);
         }
         return array;
     }
 
-    public static void max_Heapify(int[] array, int root){
-        int max = root;
-        int left = (2 * root);
-        int right = left + 1;
-        if(left <= array.length - 1 && array[left - 1] > array[max - 1])
-            max = left;
-        if(right <= array.length - 1 && array[right - 1] > array[max - 1])
-            max = right;
-        if(root != max) {
-            swap(array, max - 1, root - 1);
-            max_Heapify(array, max);
-        }
+    /**
+     *  run the max heapify algorithm, the left and right child must be a max heap.
+     *  run recursivly to check if both children are max heaps
+     *
+     * @param array
+     * @param parent    the parent node, current root of subtree
+     */
+    public static void max_Heapify(int[] array, int parent,  int right){
+        int max = parent;
+        int leftChild = (2 * parent);
+        int rightChild = leftChild + 1;
+            if (leftChild <= right && array[leftChild - 1] > array[max - 1])
+                max = leftChild;
+            if (rightChild <= right && array[rightChild - 1] > array[max - 1])
+                max = rightChild;
+            if (parent != max) {
+                swap(array, max - 1, parent - 1);
+                max_Heapify(array, max, right);
+            }
     }
 
+    /**
+     * swap value of index a with index b in array
+     * @param array
+     * @param a
+     * @param b
+     */
     public static void swap(int[] array, int a, int b){
         int temp = array[a];
         array[a] = array[b];
         array[b] = temp;
     }
 
+    /**
+     *
+     * @param min
+     * @param max
+     * @return a random int between min - max
+     */
     public static int randInt(int min, int max) {
         Random rand = new Random();
         int randomNum = rand.nextInt((max - min) + 1) + min;
