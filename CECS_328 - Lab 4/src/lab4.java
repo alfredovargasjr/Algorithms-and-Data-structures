179fabc66204f52f04ca26c1b3debecd75c00d24
Alfredo Vargas <vargas.alfredojr@gmail.com>
2017-12-09 00:04:36 -0800

    Finished MSS algorithm for O(nlogn)

--- CECS_328 - Lab 4/src/lab4.java
+++ CECS_328 - Lab 4/src/lab4.java
@@ -1,23 +1,38 @@
 import java.math.MathContext;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class lab4 {
+
+    /**
+     * compare the results of the Max Subsequence Summation of an array, using a O(n) and O(nlogn) algorithm
+     * @param args
+     */
     public static void main(String[] args){
-        int n = 5;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = in.nextInt();
         int[] array = new int[n];
         for(int i = 0; i < n; i++) {
-            array[i] = (randInt(-100, 100));
+            array[i] = randInt(-100, 100);
         }
         System.out.println(Arrays.toString(array));
         System.out.println("MSS nlogn: " + MSS_nlogn(array,0,array.length - 1));
-        System.out.println("MSS n: " + MSS_n(array));
+        System.out.println("MSS n: " + MSS_n(array, 0, array.length));
     }
 
-    public static int MSS_n(int[] a){
-        int mss = a[0];
-        int temp = a[0];
-        for(int i = 1; i < a.length; i++){
+    /**
+     * find the max subsequence summation, O(n) running time
+     * @param a
+     * @param left
+     * @param right
+     * @return  mss
+     */
+    public static int MSS_n(int[] a, int left, int right){
+        int mss = a[left];
+        int temp = a[left];
+        for(int i = left + 1; i < right; i++){
             temp = temp + a[i];
             if(temp > mss){
                 mss = temp;
@@ -33,15 +48,31 @@ public class lab4 {
         return mss;
     }
 
+    /**
+     * find the max subsequence summation, O(nlog) running time
+     * get the largest max sequence of the left tree, right tree, and center tree
+     * @param a
+     * @param left
+     * @param right
+     * @return  mss
+     */
     public static int MSS_nlogn(int[] a, int left, int right){
         if(left >= right){
             return a[left];
         }
-        int left_tree = MSS_nlogn(a, left, (left + right) / 2);
-        int right_tree = MSS_nlogn(a, ((left + right) / 2) + 1, right);
-        return Math.max(left_tree, Math.max(right_tree, left_tree + right_tree));
+        int parent = (left + right) / 2;
+        int left_tree = MSS_nlogn(a, left, parent - 1);
+        int right_tree = MSS_nlogn(a, parent + 1, right);
+        int center_tree = MSS_n(a, left, (right - left) + 1);
+        return Math.max(center_tree, Math.max(left_tree, right_tree));
     }
 
+    /**
+     * generate a random integer between min - max
+     * @param min
+     * @param max
+     * @return  random integer
+     */
     public static int randInt(int min, int max) {
         Random rand = new Random();
         int randomNum = rand.nextInt((max - min) + 1) + min;
